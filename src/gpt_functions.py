6060fee767264c284da8fc642febcd5fd14fc2dd
Tim Georg <tim_georg@t-online.de>
2024-12-25 16:57:05 +0100

    refactor gpt functions

---
+++ src/gpt_functions.py
@@ -0,0 +1,121 @@
+from openai import OpenAI
+import os
+from typing import List, Dict
+
+
+def get_chapter_summary(section: Dict, model: str = 'gpt-4o-mini') -> str:
+    """
+    Generates a summary for a given section of a video using the specified OpenAI model.
+    Args:
+        section (dict): The section of the video to summarize.
+        model (str, optional): The model to use for generating the summary. Defaults to 'gpt-4o-mini'.
+    Returns:
+        str: The generated summary of the section.
+    """
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {'role': 'system', 
+            'content': 
+                'Summarize the following section of the video. \
+                    Use the provided content to generate a summary of the section. \
+                    Stay in the original language. \
+                    Create Bullet Points, but dont shorten the idea of the content. Explain the topic briefly and not that they talk about it in the video. \
+                    If they talk about the 5 things or the 9 types or something like that, list them. \
+                    Answer the question thats given in the topic or chapter title if available. \
+                    Put the topic with timestamp (in h, min, sec) [if available] in the format "hh:mm:ss" or "mm:ss" as a heading in the format: "Heading Topic (00:34)"\
+                    Every Heading should be a markdown ## heading. If there is no heading title (eg. just Chapter 1), create a heading out of the content provided. \
+                    Try to keep it as short as possible, but as long as necessary. '},
+
+            {'role': 'user', 'content': str(section)}
+        ],
+        temperature=0.08,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stream=True
+    )
+
+    result = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            result += chunk.choices[0].delta.content
+
+    return result
+
+
+def get_minimal_chapter_summary(api_key: str, section: Dict, model: str = 'gpt-4o-mini') -> str:
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {'role': 'system', 
+            'content': 
+                'Summarize the following chapter of a podcast as short as possible in max. 1-2 bullet points.\
+                    Stay in the original language. Keep the heading.'},
+
+            {'role': 'user', 'content': str(section)}
+        ],
+        temperature=0.08,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    result = response.choices[0].message.content
+    return result
+
+
+def get_unified_summary(api_key: str, sections: List[Dict]) -> str:
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=[
+            {'role': 'system', 
+            'content': 
+                'Summarize the following outline of a podcast. \
+                    Do not only list the topics they talk about, but briefly explain every idea you mention in the summary. \
+                    Still try to keep it as short as possible. Use bullet points if possible.'},
+
+            {'role': 'user', 'content': str(sections)}
+        ],
+        temperature=0.08,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    
+    result = response.choices[0].message.content
+    return result
+
+
+def get_whole_transcript_summary(transcript: str) -> str:
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=[
+            {'role': 'system', 
+            'content': 
+                'Summarize the following transcript of a podcast. \
+                    Do not only list the topics they talk about, but briefly explain every idea you mention in the summary. \
+                    Still try to keep it as short as possible. Use bullet points if possible.'},
+
+            {'role': 'user', 'content': transcript}
+        ],
+        temperature=0.08,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    result = response.choices[0].message.content
+    return result
--- transcribe_summarize.py
+++ transcribe_summarize.py
@@ -1,19 +1,20 @@
 # Native Libraries
+import json
 import os
 import re
-import sys
-import json
 import requests
+import sys
 from datetime import datetime, timedelta
 # External Libraries
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv # pip install python-dotenv
-from youtube_transcript_api import YouTubeTranscriptApi
 from openai import OpenAI # OR: from openai import AzureOpenAI
-
+from youtube_transcript_api import YouTubeTranscriptApi
 # User-defined Libraries
+import src.gpt_functions as gpt
 from src.youtube_video import YouTubeVideo
 from Utilities.logger import Logger
+# Add Project Root to sys.path for Unit Tests
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 logger = Logger.create_logger(name=__name__)
@@ -148,120 +149,6 @@ class YouTubeTranscribeSummarize(YouTubeVideo):
         return sections
 
 
-    def get_chapter_summary(self, section, model='gpt-4o-mini'):
-
-        client = OpenAI(api_key=self.api_key)
-        self.logger.info(f"Getting summary for section: {section['topic']}")
-
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-                {'role': 'system', 
-                'content': 
-                    'Summarize the following section of the video. \
-                        Use the provided content to generate a summary of the section. \
-                        Stay in the original language. \
-                        Create Bullet Points, but dont shorten the idea of the content. Explain the topic briefly and not that they talk about it in the video. \
-                        If they talk about the 5 things or the 9 types or something like that, list them. \
-                        Answer the question thats given in the topic or chapter title if available. \
-                        Put the topic with timestamp (in h, min, sec) [if available] in the format "hh:mm:ss" or "mm:ss" as a heading in the format: "Heading Topic (00:34)"\
-                        Every Heading should be a markdown ## heading. If there is no heading title (eg. just Chapter 1), create a heading out of the content provided. \
-                        Try to keep it as short as possible, but as long as necessary. '},
-
-                {'role': 'user', 'content': str(section)}
-            ],
-            temperature=0.08,
-            max_tokens=1024,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stream=True
-        )
-
-        result = ""
-        for chunk in response:
-            if chunk.choices[0].delta.content is not None:
-                result += chunk.choices[0].delta.content
-
-        return result
-    
-
-    def get_minimal_chapter_summary(self, section, model='gpt-4o-mini'):
-
-        client = OpenAI(api_key=self.api_key)
-
-        response = client.chat.completions.create(
-            model=model,
-            messages=[
-                {'role': 'system', 
-                'content': 
-                    'Summarize the following chapter of a podcast as short as possible in max. 1-2 bullet points.\
-                        Stay in the original language. Keep the heading.'},
-
-                {'role': 'user', 'content': str(section)}
-            ],
-            temperature=0.08,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-
-        result = response.choices[0].message.content
-        return result
-
-
-    def get_unified_summary(self, sections):
-        client = OpenAI(api_key=self.api_key)
-
-        response = client.chat.completions.create(
-
-            model='gpt-4o-mini',
-            messages=[
-                {'role': 'system', 
-                'content': 
-                    'Summarize the following outline of a podcast. \
-                        Do not only list the topics they talk about, but briefly explain every idea you mention in the summary. \
-                        Still try to keep it as short as possible. Use bullet points if possible.'},
-
-                {'role': 'user', 'content': str(sections)}
-            ],
-            temperature=0.08,
-            max_tokens=512,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-        
-        result = response.choices[0].message.content
-        return result
-    
-
-    def get_whole_transcript_summary(self, transcript):
-        client = OpenAI(api_key=self.api_key)
-
-        response = client.chat.completions.create(
-            model='gpt-4o-mini',
-            messages=[
-                {'role': 'system', 
-                'content': 
-                    'Summarize the following transcript of a podcast. \
-                        Do not only list the topics they talk about, but briefly explain every idea you mention in the summary. \
-                        Still try to keep it as short as possible. Use bullet points if possible.'},
-
-                {'role': 'user', 'content': transcript}
-            ],
-            temperature=0.08,
-            max_tokens=1024,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-
-        result = response.choices[0].message.content
-        return result
-
-
     
 def get_video(url):
     video = YouTubeVideo(url)
@@ -376,7 +263,7 @@ def summary_by_chapters(url: str, api_key=os.getenv('OPENAI_API_KEY')) -> list |
     chap_summaries = []
 
     for section in sections:
-        chap_summary = obj.get_chapter_summary(section)
+        chap_summary = gpt.get_chapter_summary(section)
         chap_summaries.append(chap_summary)
 
     return chap_summaries
@@ -387,7 +274,7 @@ def summary_entire_video(url: str, api_key=os.getenv('OPENAI_API_KEY')) -> str:
     obj.get_data()
 
     unified_transcript = " ".join([item["text"] for item in obj.transcript])
-    summary = obj.get_whole_transcript_summary(unified_transcript)
+    summary = gpt.get_whole_transcript_summary(unified_transcript)
 
     return summary
 
